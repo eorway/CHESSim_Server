869a222854e36402a721ad2b195ee085ef9fd393
eorway <eor.way@web.de>
2020-01-07 09:24:58 +0100

    generated dropwizard-simple archetype

---
+++ Server/src/main/java/CHESSim/CHESSim_ServerApplication.java
@@ -0,0 +1,29 @@
+package CHESSim;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class CHESSim_ServerApplication extends Application<CHESSim_ServerConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new CHESSim_ServerApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "CHESSim_Server";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<CHESSim_ServerConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final CHESSim_ServerConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
