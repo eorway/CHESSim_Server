df8117cf7420d2777afb46a5efb8db6ac6265eb9
eorway <eor.way@web.de>
2020-01-10 13:05:54 +0100

    added LobbyHealthcheck

---
+++ Server/src/main/java/CHESSim/health/LobbyHealthCheck.java
@@ -0,0 +1,16 @@
+package CHESSim.health;
+
+import com.codahale.metrics.health.HealthCheck;
+
+public class LobbyHealthCheck extends HealthCheck {
+
+	
+	
+	
+	@Override
+	protected Result check() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
