664d6401214c795f9eb2ee5455392350be2e0fd0
eorway <eor.way@web.de>
2020-01-10 10:18:51 +0100

    updated Produces and added sample json

--- Server/src/main/java/CHESSim/resources/LobbyResource.java
+++ Server/src/main/java/CHESSim/resources/LobbyResource.java
@@ -2,15 +2,27 @@ package CHESSim.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-@Path("/Lobbys")
+@Path("/Lobbies")
 public class LobbyResource{
 	
 	@GET
 	@Path("all")
+	@Produces(MediaType.APPLICATION_JSON)
 	public String getLobbys()
 	{
-		return "{json:\"i'm a json\"}";
+		return "[\n" + 
+				"    {\n" + 
+				"        \"name\": \"Meine Session\",\n" + 
+				"        \"password\": null\n" + 
+				"    },\n" + 
+				"    {\n" + 
+				"        \"name\": \"Seine Session\",\n" + 
+				"        \"password\": null\n" + 
+				"    }\n" + 
+				"]";
 	}
 	
 }
