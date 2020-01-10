7369fe61fedcbfc69142c3d0e8b2f5a8aedfa019
eorway <eor.way@web.de>
2020-01-10 10:17:57 +0100

    sample Resource for Games

--- Server/src/main/java/CHESSim/resources/GameResource.java
+++ Server/src/main/java/CHESSim/resources/GameResource.java
@@ -1,5 +1,36 @@
 package CHESSim.resources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/Game")
 public class GameResource {
 
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getGameInfo(@PathParam("id") int id )
+	{
+		return "{\n" + 
+				"	\"board "+id+"\": [\n" + 
+				"		[0,0,0,0,0,0,0,0],\n" + 
+				"		[0,0,0,0,0,0,0,0],\n" + 
+				"		[0,0,0,0,0,0,0,0],\n" + 
+				"		[0,0,0,0,0,0,0,0],\n" + 
+				"		[0,0,0,0,0,0,0,0],\n" + 
+				"		[0,0,0,0,0,0,0,0],\n" + 
+				"		[0,0,0,0,0,0,0,0],\n" + 
+				"		[0,0,0,0,0,0,0,0]\n" + 
+				"	],\n" + 
+				"	\"age\": 1033,\n" + 
+				"	\"lastmove\": 0,\n" + 
+				"	\"active_team\": 1\n" + 
+				"}";
+	}
+	
+	
+	
 }
