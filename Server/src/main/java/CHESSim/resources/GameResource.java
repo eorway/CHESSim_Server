409249ade22e46f70a28ae36835063961d02a97d
eorway <eor.way@web.de>
2020-01-10 12:39:35 +0100

    minor changes

--- Server/src/main/java/CHESSim/resources/GameResource.java
+++ Server/src/main/java/CHESSim/resources/GameResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/Game")
+@Path("/game")
 public class GameResource {
 
 	@GET
--- Server/src/main/java/CHESSim/resources/LobbyResource.java
+++ Server/src/main/java/CHESSim/resources/LobbyResource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/Lobbies")
+@Path("/lobbies")
 public class LobbyResource{
 	
 	@GET
